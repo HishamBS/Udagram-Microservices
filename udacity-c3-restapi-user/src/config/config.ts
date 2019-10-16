3c3e8277080f2d83d90220850f82eacc2157abf5
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2019-10-16 20:58:13 +0300

    final version with working k8s

--- udacity-c3-restapi-user/src/config/config.ts
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -11,11 +11,15 @@ export const config = {
     "url": process.env.URL
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DB,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET,
+    "url": process.env.URL
   },
   "jwt": {
     "secret": process.env.JWT_SECRET
